e8f760eea8e9675f2ed7cece693b1b1fd213ba6c
brukal001 <90138807+brukal001@users.noreply.github.com>
2025-09-30 10:38:00 +1000

    Update api.dart

--- Font_end/cryptx_app/lib/api.dart
+++ Font_end/cryptx_app/lib/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  static const base = 'http://10.0.2.2:8000/api'; // emulator localhost
+  static const base = 'https://cryptx-gj55.onrender.com'; // emulator localhost
   String? access;
 
   Map<String, String> _authJson() {
